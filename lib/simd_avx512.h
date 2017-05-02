59c22a2890c7a3a5a9749c4b0fcaa775afd9cf23
JeWaVe <regis.portalez@altimesh.com>
2017-05-02 16:41:32 +0200

    fix typo on 256

--- lib/simd_avx512.h
+++ lib/simd_avx512.h
@@ -5,7 +5,7 @@
 * https://opensource.org/licenses/MIT
 ***/
 
-// this header defines SIMD MACROS for avx256 intrinsics
+// this header defines SIMD MACROS for avx512 intrinsics
 // used to generate avx512 code from generic SIMD code (sha1_simd.cinc, ubc_check_simd.cinc)
 
 #ifndef SIMD_AVX512_HEADER
@@ -34,11 +34,11 @@
 #define SIMD_OR_VW(l,r)		_mm512_or_si512(l, _mm512_set1_epi32(r))
 #define SIMD_XOR_VV(l,r)	_mm512_xor_si512(l,r)
 #define SIMD_XOR_VW(l,r)	_mm512_xor_si512(l, _mm512_set1_epi32(r))
-//#define SIMD_NOT_V(l)		_mm512_andnot_si256(l,l)
+//#define SIMD_NOT_V(l)		_mm512_andnot_si512(l,l)
 #define SIMD_SHL_V(l,i)		_mm512_slli_epi32(l,i)
 #define SIMD_SHR_V(l,i)		_mm512_srli_epi32(l,i)
-//#define SIMD_ROL_V(l,i)		_mm256_rol_epi32(l,i)
-//#define SIMD_ROR_V(l,i)		_mm256_ror_epi32(l,i)
+//#define SIMD_ROL_V(l,i)		_mm512_rol_epi32(l,i)
+//#define SIMD_ROR_V(l,i)		_mm512_ror_epi32(l,i)
 #define SIMD_CLEANUP
 
 #else // __GNUC__
@@ -55,17 +55,17 @@
 #define SIMD_ADD_VW(l,r)	_mm512_add_epi32(l, _mm512_set1_epi32(r))
 #define SIMD_SUB_VV(l,r)	_mm512_sub_epi32(l,r)
 #define SIMD_SUB_VW(l,r)	_mm512_sub_epi32(l, _mm512_set1_epi32(r))
-#define SIMD_AND_VV(l,r)	_mm512_and_si256(l,r)
-#define SIMD_AND_VW(l,r)	_mm512_and_si256(l, _mm512_set1_epi32(r))
-#define SIMD_OR_VV(l,r)		_mm512_or_si256(l,r)
-#define SIMD_OR_VW(l,r)		_mm512_or_si256(l, _mm512_set1_epi32(r))
-#define SIMD_XOR_VV(l,r)	_mm512_xor_si256(l,r)
-#define SIMD_XOR_VW(l,r)	_mm512_xor_si256(l, _mm512_set1_epi32(r))
+#define SIMD_AND_VV(l,r)	_mm512_and_si512(l,r)
+#define SIMD_AND_VW(l,r)	_mm512_and_si512(l, _mm512_set1_epi32(r))
+#define SIMD_OR_VV(l,r)		_mm512_or_si512(l,r)
+#define SIMD_OR_VW(l,r)		_mm512_or_si512(l, _mm512_set1_epi32(r))
+#define SIMD_XOR_VV(l,r)	_mm512_xor_si512(l,r)
+#define SIMD_XOR_VW(l,r)	_mm512_xor_si512(l, _mm512_set1_epi32(r))
 //#define SIMD_NOT_V(l)		_mm512_andnot_si512(l,l)
 #define SIMD_SHL_V(l,i)		_mm512_slli_epi32(l,i)
 #define SIMD_SHR_V(l,i)		_mm512_srli_epi32(l,i)
-//#define SIMD_ROL_V(l,i)		_mm256_rol_epi32(l,i)
-//#define SIMD_ROR_V(l,i)		_mm256_ror_epi32(l,i)
+//#define SIMD_ROL_V(l,i)		_mm512_rol_epi32(l,i)
+//#define SIMD_ROR_V(l,i)		_mm512_ror_epi32(l,i)
 #define SIMD_CLEANUP
 
 #endif // __GNUC__
