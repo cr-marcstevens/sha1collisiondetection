d96612d26c2afb3ac78b457fe9f22fc9c336405d
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2017-05-18 21:38:43 +0000

    Allow for defining SHA1DC_ALLOW_UNALIGNED_ACCESS externally
    See the preceding commit ("Amend the lib/ code for easier inclusion in
    other programs", 2017-05-17) for why & rationale. The same applies
    here, except for unaligned access instead of "is this processor
    Big-Endian?".

--- lib/sha1.c
+++ lib/sha1.c
@@ -42,16 +42,15 @@
 #endif
 #endif
 
+#ifndef SHA1DC_ALLOW_UNALIGNED_ACCESS
 #if (defined(__amd64__) || defined(__amd64) || defined(__x86_64__) || defined(__x86_64) || \
      defined(i386) || defined(__i386) || defined(__i386__) || defined(__i486__)  || \
      defined(__i586__) || defined(__i686__) || defined(_M_IX86) || defined(__X86__) || \
      defined(_X86_) || defined(__THW_INTEL__) || defined(__I86__) || defined(__INTEL__) || \
      defined(__386) || defined(_M_X64) || defined(_M_AMD64))
-
 #define SHA1DC_ALLOW_UNALIGNED_ACCESS
-
-#endif /*UNALIGNMENT DETECTION*/
-
+#endif
+#endif
 
 #define rotate_right(x,n) (((x)>>(n))|((x)<<(32-(n))))
 #define rotate_left(x,n)  (((x)<<(n))|((x)>>(32-(n))))
