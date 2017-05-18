01dc6c3c2143dcc7396dbce77a7169328d495178
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2017-05-18 21:38:43 +0000

    Allow for defining SHA1DC_FORCE_UNALIGNED_ACCESS externally
    This allows for doing -DSHA1DC_FORCE_UNALIGNED_ACCESS on a
    platform/CPU that's not listed here, since the default is to fall back
    to aligned access unless except on a whitelist of CPUs.
    
    It would be nice e.g. for the git project if someone running on such a
    platform didn't have to monkeypatch the code, instead they can just
    provide a -DSHA1DC_FORCE_UNALIGNED_ACCESS argument

--- lib/sha1.c
+++ lib/sha1.c
@@ -45,7 +45,8 @@
 
 #endif /*ENDIANNESS SELECTION*/
 
-#if (defined(__amd64__) || defined(__amd64) || defined(__x86_64__) || defined(__x86_64) || \
+#if (defined SHA1DC_FORCE_UNALIGNED_ACCESS || \
+     defined(__amd64__) || defined(__amd64) || defined(__x86_64__) || defined(__x86_64) || \
      defined(i386) || defined(__i386) || defined(__i386__) || defined(__i486__)  || \
      defined(__i586__) || defined(__i686__) || defined(_M_IX86) || defined(__X86__) || \
      defined(_X86_) || defined(__THW_INTEL__) || defined(__I86__) || defined(__INTEL__) || \
