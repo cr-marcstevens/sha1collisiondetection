c09e03e7241607dd495f76c6720bcf4deaba2cd9
SODA Noriyuki <soda@NetBSD.org>
2017-05-25 14:49:05 +0900

    use "_BIG_ENDIAN" symbol on Solaris
    revive the "_BIG_ENDIAN" symbol support on Solaris,
    which was removed in commit 947dd3589ea4
    pointed out by obache-san

--- lib/sha1.c
+++ lib/sha1.c
@@ -39,6 +39,7 @@
     ((defined(__BYTE_ORDER) && (__BYTE_ORDER == __BIG_ENDIAN)) || \
     (defined(__BYTE_ORDER__) && (__BYTE_ORDER__ == __BIG_ENDIAN__)) || \
     (defined(_BYTE_ORDER) && (_BYTE_ORDER == _BIG_ENDIAN)) || \
+    (defined(__sun) && defined(_BIG_ENDIAN)) || \
     defined(__BIG_ENDIAN__) || defined(__ARMEB__) || defined(__THUMBEB__) ||  defined(__AARCH64EB__) || \
     defined(_MIPSEB) || defined(__MIPSEB) || defined(__MIPSEB__) || defined(SHA1DC_FORCE_BIGENDIAN))
 
