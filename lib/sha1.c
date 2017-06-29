cfd84f8425554d0214e23c50ca4764cb78ba3a0e
SODA Noriyuki <soda@NetBSD.org>
2017-06-30 01:08:01 +0900

    fix unintended fallthrough for *BSD/newlib case
    pointed out by @cr-marcstevens in
    https://github.com/cr-marcstevens/sha1collisiondetection/pull/34#issuecomment-311650322

--- lib/sha1.c
+++ lib/sha1.c
@@ -72,18 +72,16 @@
 
 #else /* Not under GCC-alike or glibc */
 
+#if defined(_BYTE_ORDER) && defined(_BIG_ENDIAN) && defined(_LITTLE_ENDIAN)
 /*
  * *BSD and newlib (embeded linux, cygwin, etc).
  * the defined(_BIG_ENDIAN) && defined(_LITTLE_ENDIAN) part prevents
  * this condition from matching with Solaris/sparc.
  * (Solaris defines only one endian macro)
- * 
  */
-#if defined(_BYTE_ORDER) && \
-    defined(_BIG_ENDIAN) && defined(_LITTLE_ENDIAN) &&	\
-    _BYTE_ORDER == _BIG_ENDIAN
-
+#if _BYTE_ORDER == _BIG_ENDIAN
 #define SHA1DC_BIGENDIAN
+#endif
 
 #else /* Not under GCC-alike or glibc or *BSD or newlib or <processor whitelist> */
 
