e96045203c0dfe73016ef06d8bd30051ec0b9849
Sevan Janiyan <venture37@geeklan.co.uk>
2023-04-14 21:06:56 +0100

    lib/sha1.c: Set SHA1DC_BIGENDIAN on OS X PowerPC
    Otherwise it's built in little endian mode.

--- lib/sha1.c
+++ lib/sha1.c
@@ -92,6 +92,10 @@
  */
 #define SHA1DC_BIGENDIAN
 
+#elif (defined(__APPLE__) && defined(__BIG_ENDIAN__) && !defined(SHA1DC_BIGENDIAN))
+/* older gcc compilers which are the default  on Apple PPC do not define __BYTE_ORDER__ */
+#define SHA1DC_BIGENDIAN
+
 /* Not under GCC-alike or glibc or *BSD or newlib or <processor whitelist> */
 #elif (defined(_AIX) || defined(__hpux))
 
